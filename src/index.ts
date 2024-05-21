2ce47fa417d79b4659eeb6d61932f31cf5787851
juji <jujiyangasli@gmail.com>
2024-05-22 06:45:25 +0700

    docs

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,43 @@
 /**
- * Cookie
- * testing 
+ * Simple cookie parser & serializer
  * 
- * @module simpleCookie
+ * ```ts
+ * import { stringify, parse, tokenize } from 'simple-cookie';
+ * 
+ * const cookieObject = {
+ *	name: 'cookieName',
+ *	value: 'cookie value',
+ *	expires: (new Date()).valueOf() + 500000,
+ *	path: '/',
+ *	domain: 'domain.com',
+ *	httponly: false,
+ *	secure: true,
+ *	samesite: 'cookie samesite'
+ * }
+ * 
+ * const cookie: string = stringify( cookieObject );
+ * // cookieName=cookie%20value; Expires: Sat, 15-Aug-2015 17:41:05 GMT; Max-Age: 31449600; Path=/; domain=domain.com; secure; samesite=None
+ * 
+ * 
+ * const cookieObject: CookieObject = parse( cookieString  [, defaultPath]  [, defaultDomain]  );
+ * // will create object like the 'cookieObject'
+ * 
+ * 
+ * const tokens: string = tokenize([
+ * 	{name:'cookie1', value: 'cvalue1'},
+ * 	{name:'cookie2', value: 'cvalue2'},
+ * 	{name:'cookie3', value: 'cvalue3'}
+ * ]);
+ * // cookie1=cvalue1; cookie2=cvalue2; cookie3=cvalue3
+ * 
+ * ```
+ * 
+ * @module
  */
 
 
 /** This function returns Expiry date in the format of Expires=;Max-Age=. */
-function printExpires(expires: string|number|Date|boolean){
+function printExpires(expires: string|number|Date|boolean): string | false {
   if(!expires) return false;
   let date = new Date();
   if(typeof expires === 'string') date = new Date(expires);
